d138a885979256db824870f6f2268b3ee81fe699
Jay Pilgreen <jpilgreen@mcclatchy.com>
2018-01-08 11:36:52 -0600

    tttx season toggle

--- title-town-tx/src/scripts.js
+++ title-town-tx/src/scripts.js
@@ -37,10 +37,6 @@ videojs("mainPlayer").ready(function() {
 function swapVideo(id) {
   player.catalog.getVideo(id, function(err, video) {
     player.catalog.load(video);
-
-    window.parent.postMessage({
-      sentinel: 'video-scroll'
-    }, '*');
   });
 }
 
@@ -53,5 +49,31 @@ for(i = 0, len = videos.length; i < len; i++) {
   var v = videos[i];
   v.addEventListener("click", function() {
     swapVideo(this.dataset.id);
+
+    window.parent.postMessage({
+      sentinel: 'video-scroll'
+    }, '*');
   });
 }
+
+/**
+ * Season selector event
+ */
+
+var seasonSelector = document.querySelector(".seasons");
+seasonSelector.addEventListener("click", function(e) {
+  season = e.target.dataset.id;
+  if(season) {
+    // swap selector
+    seasonSelector.dataset.active = season;
+
+    // swap playlist
+    document.querySelector(".playlists").dataset.active = season;
+
+    // swap to first video
+    swapVideo(document.querySelector(`.playlist[data-id=${season}]`).querySelector('.video').dataset.id);
+
+    // resend the new height
+    sendHeight();
+  };
+})
